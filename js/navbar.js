3890f12b1095307c3376a2e7f5a9e022468c2a85
Mantra Digital Indonesia <inbox.agusmunandar@gmail.com>
2026-02-03 10:21:05 +0700

    Delete js/navbar.js

--- js/navbar.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll(".dropdown > a").forEach(menu => {
-  menu.addEventListener("click", e => {
-    e.preventDefault();
-    menu.nextElementSibling.classList.toggle("show");
-  });
-});
